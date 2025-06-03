cfa33f4d9a88e634cf5bf2380bb7ade555cc2594
Dan K. Snelson <dan@snelson.us>
2025-06-02 20:54:24 -0400

    Resources: Extension Attribute for DDM-generated Enforced Install Date (0.0.1)
    Signed-off-by: Dan K. Snelson <dan@snelson.us>

---
+++ resources/Enforced_Install_Date.zsh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+###############################################################
+# A script to determine a DDM-generated Enforced Install Date #
+# within the last numberOfDays.                               #
+# If the no matches are found, "None" will be returned.       #
+###############################################################
+
+scriptVersion="0.0.1"
+
+numberOfDays=7
+
+ddmEnforcedInstallDate=$( awk -v date=$(date -v-"$numberOfDays"d +%Y-%m-%d) '$1 >= date' /var/log/install.log | grep EnforcedInstallDate | sed -n 's/.*EnforcedInstallDate:\([^|]*\)|VersionString:\([^|]*\)|BuildVersionString:\([^|]*\).*/\1 macOS \2 (\3)/p' | sort -u )
+
+if [[ -z "$ddmEnforcedInstallDate" ]]; then
+	RESULT="None"
+else
+	RESULT="$ddmEnforcedInstallDate"
+fi
+
+/bin/echo "<result>${RESULT}</result>"
+
+exit 0
